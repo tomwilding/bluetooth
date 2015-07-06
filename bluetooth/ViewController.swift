0bda963c7aa959bc5b0b0ebec7b3c15345b43bb7
thomas.wilding <thomas.wilding@accenture.com>
2015-07-06 10:21:31 +0100

    tracking coords with filtering

--- bluetooth/ViewController.swift
+++ bluetooth/ViewController.swift
@@ -7,17 +7,66 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    
+    var xs = [Double]()
+    var ys = [Double]()
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    lazy var motionManager = CMMotionManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("View")
+        
+        motionManager.deviceMotionUpdateInterval = 0.01
+        
+        if motionManager.deviceMotionAvailable{
+            let queue = NSOperationQueue()
+            motionManager.startDeviceMotionUpdatesToQueue(queue, withHandler: {
+                data, error in
+                self.handleDeviceMotionUpdate(data)
+            })
+        } else {
+            print("Accelerometer is not available")
+        }
+    }
+    
+    func handleDeviceMotionUpdate(data : CMDeviceMotion) {
+        var x = data.userAcceleration.x * -10000
+        var y = data.userAcceleration.y * 10000
+        if (abs(x) < 500) {
+            x = 0        }
+        if (abs(y) < 500) {
+            y = 0
+        }
+        xs.append(x)
+        ys.append(y)
+    }
+    
+    func printArray(xs: [Double]) {
+        for x in xs {
+            println(x)
+        }
+    }
+    
+    @IBAction func redTapped(sender : AnyObject) {
+        for x in xs {
+            print("\(x),")
+        }
+        println()
+        println()
+        println()
+        for y in ys {
+            print("\(y),")
+        }
     }
 
 
